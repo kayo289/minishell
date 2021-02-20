d2168bb32c8bb4e99cf541a47a6b87241c5348c1
rishimot <rishimot@student.42.tokyo>
2021-02-20 08:00:28 +0100

    add pipe_testcase && result

---
+++ pipe_testcases.sh
@@ -0,0 +1,360 @@
+echo "cd | pwd";
+cd | pwd;
+echo ""
+echo "cd | unset";
+cd | unset;
+echo ""
+echo "pwd | unset";
+pwd | unset;
+echo ""
+echo "cd | pwd | unset";
+cd | pwd | unset;
+echo ""
+echo "cd | exit";
+cd | exit;
+echo ""
+echo "pwd | exit";
+pwd | exit;
+echo ""
+echo "cd | pwd | exit";
+cd | pwd | exit;
+echo ""
+echo "unset | exit";
+unset | exit;
+echo ""
+echo "cd | unset | exit";
+cd | unset | exit;
+echo ""
+echo "pwd | unset | exit";
+pwd | unset | exit;
+echo ""
+echo "cd | pwd | unset | exit";
+cd | pwd | unset | exit;
+echo ""
+echo "cd | export";
+cd | export;
+echo ""
+echo "pwd | export";
+pwd | export;
+echo ""
+echo "cd | pwd | export";
+cd | pwd | export;
+echo ""
+echo "unset | export";
+unset | export;
+echo ""
+echo "cd | unset | export";
+cd | unset | export;
+echo ""
+echo "pwd | unset | export";
+pwd | unset | export;
+echo ""
+echo "cd | pwd | unset | export";
+cd | pwd | unset | export;
+echo ""
+echo "exit | export";
+exit | export;
+echo ""
+echo "cd | exit | export";
+cd | exit | export;
+echo ""
+echo "pwd | exit | export";
+pwd | exit | export;
+echo ""
+echo "cd | pwd | exit | export";
+cd | pwd | exit | export;
+echo ""
+echo "unset | exit | export";
+unset | exit | export;
+echo ""
+echo "cd | unset | exit | export";
+cd | unset | exit | export;
+echo ""
+echo "pwd | unset | exit | export";
+pwd | unset | exit | export;
+echo ""
+echo "cd | pwd | unset | exit | export";
+cd | pwd | unset | exit | export;
+echo ""
+echo "cd | echo";
+cd | echo;
+echo ""
+echo "pwd | echo";
+pwd | echo;
+echo ""
+echo "cd | pwd | echo";
+cd | pwd | echo;
+echo ""
+echo "unset | echo";
+unset | echo;
+echo ""
+echo "cd | unset | echo";
+cd | unset | echo;
+echo ""
+echo "pwd | unset | echo";
+pwd | unset | echo;
+echo ""
+echo "cd | pwd | unset | echo";
+cd | pwd | unset | echo;
+echo ""
+echo "exit | echo";
+exit | echo;
+echo ""
+echo "cd | exit | echo";
+cd | exit | echo;
+echo ""
+echo "pwd | exit | echo";
+pwd | exit | echo;
+echo ""
+echo "cd | pwd | exit | echo";
+cd | pwd | exit | echo;
+echo ""
+echo "unset | exit | echo";
+unset | exit | echo;
+echo ""
+echo "cd | unset | exit | echo";
+cd | unset | exit | echo;
+echo ""
+echo "pwd | unset | exit | echo";
+pwd | unset | exit | echo;
+echo ""
+echo "cd | pwd | unset | exit | echo";
+cd | pwd | unset | exit | echo;
+echo ""
+echo "export | echo";
+export | echo;
+echo ""
+echo "cd | export | echo";
+cd | export | echo;
+echo ""
+echo "pwd | export | echo";
+pwd | export | echo;
+echo ""
+echo "cd | pwd | export | echo";
+cd | pwd | export | echo;
+echo ""
+echo "unset | export | echo";
+unset | export | echo;
+echo ""
+echo "cd | unset | export | echo";
+cd | unset | export | echo;
+echo ""
+echo "pwd | unset | export | echo";
+pwd | unset | export | echo;
+echo ""
+echo "cd | pwd | unset | export | echo";
+cd | pwd | unset | export | echo;
+echo ""
+echo "exit | export | echo";
+exit | export | echo;
+echo ""
+echo "cd | exit | export | echo";
+cd | exit | export | echo;
+echo ""
+echo "pwd | exit | export | echo";
+pwd | exit | export | echo;
+echo ""
+echo "cd | pwd | exit | export | echo";
+cd | pwd | exit | export | echo;
+echo ""
+echo "unset | exit | export | echo";
+unset | exit | export | echo;
+echo ""
+echo "cd | unset | exit | export | echo";
+cd | unset | exit | export | echo;
+echo ""
+echo "pwd | unset | exit | export | echo";
+pwd | unset | exit | export | echo;
+echo ""
+echo "cd | pwd | unset | exit | export | echo";
+cd | pwd | unset | exit | export | echo;
+echo ""
+echo "cd | env";
+cd | env;
+echo ""
+echo "pwd | env";
+pwd | env;
+echo ""
+echo "cd | pwd | env";
+cd | pwd | env;
+echo ""
+echo "unset | env";
+unset | env;
+echo ""
+echo "cd | unset | env";
+cd | unset | env;
+echo ""
+echo "pwd | unset | env";
+pwd | unset | env;
+echo ""
+echo "cd | pwd | unset | env";
+cd | pwd | unset | env;
+echo ""
+echo "exit | env";
+exit | env;
+echo ""
+echo "cd | exit | env";
+cd | exit | env;
+echo ""
+echo "pwd | exit | env";
+pwd | exit | env;
+echo ""
+echo "cd | pwd | exit | env";
+cd | pwd | exit | env;
+echo ""
+echo "unset | exit | env";
+unset | exit | env;
+echo ""
+echo "cd | unset | exit | env";
+cd | unset | exit | env;
+echo ""
+echo "pwd | unset | exit | env";
+pwd | unset | exit | env;
+echo ""
+echo "cd | pwd | unset | exit | env";
+cd | pwd | unset | exit | env;
+echo ""
+echo "export | env";
+export | env;
+echo ""
+echo "cd | export | env";
+cd | export | env;
+echo ""
+echo "pwd | export | env";
+pwd | export | env;
+echo ""
+echo "cd | pwd | export | env";
+cd | pwd | export | env;
+echo ""
+echo "unset | export | env";
+unset | export | env;
+echo ""
+echo "cd | unset | export | env";
+cd | unset | export | env;
+echo ""
+echo "pwd | unset | export | env";
+pwd | unset | export | env;
+echo ""
+echo "cd | pwd | unset | export | env";
+cd | pwd | unset | export | env;
+echo ""
+echo "exit | export | env";
+exit | export | env;
+echo ""
+echo "cd | exit | export | env";
+cd | exit | export | env;
+echo ""
+echo "pwd | exit | export | env";
+pwd | exit | export | env;
+echo ""
+echo "cd | pwd | exit | export | env";
+cd | pwd | exit | export | env;
+echo ""
+echo "unset | exit | export | env";
+unset | exit | export | env;
+echo ""
+echo "cd | unset | exit | export | env";
+cd | unset | exit | export | env;
+echo ""
+echo "pwd | unset | exit | export | env";
+pwd | unset | exit | export | env;
+echo ""
+echo "cd | pwd | unset | exit | export | env";
+cd | pwd | unset | exit | export | env;
+echo ""
+echo "echo | env";
+echo | env;
+echo ""
+echo "cd | echo | env";
+cd | echo | env;
+echo ""
+echo "pwd | echo | env";
+pwd | echo | env;
+echo ""
+echo "cd | pwd | echo | env";
+cd | pwd | echo | env;
+echo ""
+echo "unset | echo | env";
+unset | echo | env;
+echo ""
+echo "cd | unset | echo | env";
+cd | unset | echo | env;
+echo ""
+echo "pwd | unset | echo | env";
+pwd | unset | echo | env;
+echo ""
+echo "cd | pwd | unset | echo | env";
+cd | pwd | unset | echo | env;
+echo ""
+echo "exit | echo | env";
+exit | echo | env;
+echo ""
+echo "cd | exit | echo | env";
+cd | exit | echo | env;
+echo ""
+echo "pwd | exit | echo | env";
+pwd | exit | echo | env;
+echo ""
+echo "cd | pwd | exit | echo | env";
+cd | pwd | exit | echo | env;
+echo ""
+echo "unset | exit | echo | env";
+unset | exit | echo | env;
+echo ""
+echo "cd | unset | exit | echo | env";
+cd | unset | exit | echo | env;
+echo ""
+echo "pwd | unset | exit | echo | env";
+pwd | unset | exit | echo | env;
+echo ""
+echo "cd | pwd | unset | exit | echo | env";
+cd | pwd | unset | exit | echo | env;
+echo ""
+echo "export | echo | env";
+export | echo | env;
+echo ""
+echo "cd | export | echo | env";
+cd | export | echo | env;
+echo ""
+echo "pwd | export | echo | env";
+pwd | export | echo | env;
+echo ""
+echo "cd | pwd | export | echo | env";
+cd | pwd | export | echo | env;
+echo ""
+echo "unset | export | echo | env";
+unset | export | echo | env;
+echo ""
+echo "cd | unset | export | echo | env";
+cd | unset | export | echo | env;
+echo ""
+echo "pwd | unset | export | echo | env";
+pwd | unset | export | echo | env;
+echo ""
+echo "cd | pwd | unset | export | echo | env";
+cd | pwd | unset | export | echo | env;
+echo ""
+echo "exit | export | echo | env";
+exit | export | echo | env;
+echo ""
+echo "cd | exit | export | echo | env";
+cd | exit | export | echo | env;
+echo ""
+echo "pwd | exit | export | echo | env";
+pwd | exit | export | echo | env;
+echo ""
+echo "cd | pwd | exit | export | echo | env";
+cd | pwd | exit | export | echo | env;
+echo ""
+echo "unset | exit | export | echo | env";
+unset | exit | export | echo | env;
+echo ""
+echo "cd | unset | exit | export | echo | env";
+cd | unset | exit | export | echo | env;
+echo ""
+echo "pwd | unset | exit | export | echo | env";
+pwd | unset | exit | export | echo | env;
+echo ""
+echo "cd | pwd | unset | exit | export | echo | env";
+cd | pwd | unset | exit | export | echo | env;
+echo ""
