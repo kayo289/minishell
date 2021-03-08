b115d4d41a71a66628d1cf4a996a4bda703032d0
kayo289 <kayokapi@icloud.com>
2021-03-08 20:12:08 +0900

    add ft_setenv

--- src/cd.c
+++ src/cd.c
@@ -1,13 +1,53 @@
 #include "../includes/minishell.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+static int ft_setenv(char *name, char *value)
+{
+	extern char **environ;
+	char **str;
+	char *tmp;
+	int i;
+
+	i = 0;
+	if (name == NULL || ft_strchr(name, '=') != NULL)
+		return (-1);
+	tmp = ft_strjoin(name, "=");
+	tmp = ft_strjoin(tmp, value);
+	while (environ[i] != NULL)
+	{
+		str = ft_split(environ[i], '=');
+		if (equal(str[0], name) == true)
+		{
+			environ[i] = tmp;
+			return (0);
+		}
+		i++;
+	}
+	environ[i] = ft_strdup(tmp);
+	return (0);
+}
 
 int main(int argc, char **argv)
 {
-	int i = 0;
+	char *path;
+	char *old_path;
 
 	printf("call cd\n");
-	while (i < argc){
-		printf("argv[%d]: %s\n", i , argv[i]);
-		i++;
+	old_path = getcwd(NULL, 0);
+	if (argv[1] == NULL)
+		path = get_env_value("HOME");
+	if (chdir(path) != 0)
+		ft_putstr_fd("cd: no such file or directory", 2);
+	else
+	{
+		ft_setenv("OLDPWD", old_path);
+		path = getcwd(NULL, 0);
+		ft_setenv("PWD", path);
+		free(path);
 	}
-	exit(EXIT_SUCCESS);
+	free(old_path);
+	printf("現在地:%s\n", getcwd(NULL, 0));
+	return (0);
 }
