9816259587ad12757b5b4d26981ac087da617af7
kayo289 <kayokapi@icloud.com>
2021-03-17 01:53:43 +0900

    finish echo func

--- src/echo.c
+++ src/echo.c
@@ -1,13 +1,40 @@
 #include "../includes/minishell.h"
 
-int main(int argc, char **argv)
+int find_except_c(char *argv, char c)
 {
-	int i = 0;
+	int		i;
 
-	printf("call echo\n");
-	while (i < argc){
-		printf("argv[%d]: %s\n", i , argv[i]);
+	i = 0;
+	if (c == '\0')
+		return (1);
+	while (argv[i] != '\0')
+	{
+		if (argv[i] != c)
+			return (1);
 		i++;
 	}
-	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int n_opt;
+
+	i = 0;
+	n_opt = 0;
+	while (++i < argc){
+		if (argv[i][0] == '-')
+		{
+			if (find_except_c(&argv[i][1], 'n') == 0)
+			{
+				n_opt = 1;
+				continue;
+			}
+		}
+		ft_putstr_fd(argv[i], 1);
+	}
+	if (n_opt == 0)
+		ft_putstr_fd("\n", 1);
+	exit(0);
 }
