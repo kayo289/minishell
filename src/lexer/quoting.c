e89b7692010458b34cb81b442a8d4786f7735a8a
rishimot <rishimot@student.42.tokyo>
2021-03-16 16:09:33 +0100

    not expand parameter in single quoting

--- src/lexer/quoting.c
+++ src/lexer/quoting.c
@@ -3,7 +3,7 @@
 #define MESSAGE1 "minishell: unexpected EOF while looking for matching "
 #define MESSAGE2 "minishell: syntax error: unexpected end of file"
 
-static void quote(char **line, t_ip *ip, char find_ch)
+static void double_quote(char **line, t_ip *ip, char find_ch)
 {
 	char *line2;
 
@@ -28,6 +28,29 @@ static void quote(char **line, t_ip *ip, char find_ch)
 	}
 }
 
+static void single_quote(char **line, t_ip *ip, char find_ch)
+{
+	char *line2;
+
+	while (next_ch(*line, ip) != find_ch)
+	{
+		if (ip->ch == '\0')
+		{
+			write(1, "> ", 2);
+			if (get_next_line(0, &line2) == 0)
+			{
+				error2(MESSAGE1, find_ch);
+				error2(MESSAGE2, '\0');
+				exit(1);
+			}
+			*line = ft_strjoin(*line, "\n");
+			*line = ft_strjoin(*line, line2);
+		}
+		else
+			ft_charjoin(&ip->id_string, ip->ch);
+	}
+}
+
 static void escape_character(char **line, t_ip *ip)
 {
 	char *line2;
@@ -46,9 +69,9 @@ static void escape_character(char **line, t_ip *ip)
 void quoting(char **line, t_ip *ip)
 {
 	if (ip->ch == '\"')
-		quote(line, ip, '\"');
+		double_quote(line, ip, '\"');
 	else if (ip->ch == '\'')
-		quote(line, ip, '\'');
+		single_quote(line, ip, '\'');
 	else if (ip->ch == '\\')
 		escape_character(line, ip);
 }
