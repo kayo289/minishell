dc43aa5cb81adc0e222b9ad85d88375c179361fe
rishimot <rishimot@student.42.tokyo>
2021-03-26 08:20:15 +0100

    fix insert string

--- includes/prompt.h
+++ includes/prompt.h
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <termios.h>
+#include <termcap.h>
 #include <ctype.h>
 #include "libft.h"
 
-#define ESC 0x1b
-#define LF 0x0a
+#define ESC	0x1b
+#define LF	0x0a
 #define DEL 0x7f
+#define ETX 3
+#define EOT 4
 
 typedef struct s_pos
 {
