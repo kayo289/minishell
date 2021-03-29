0b5a79c067992bb8c51692fbfcec81d58d2c5bc4
rishimot <rishimot@student.42.tokyo>
2021-03-29 19:38:11 +0200

    fix multiline of quoting

--- src/lexer/quoting.c
+++ src/lexer/quoting.c
@@ -3,34 +3,6 @@
 #define MESSAGE1 "minishell: unexpected EOF while looking for matching "
 #define MESSAGE2 "minishell: syntax error: unexpected end of file"
 
-static void double_quote(t_dlist **line, t_ip *ip)
-{
-	char *val;
-
-	while (next_ch(line, ip) != '\"')
-	{
-		if (ip->ch == '\0')
-			break;
-		if (ip->ch == '$')
-		{
-			val = expand_parameter(line);
-			ip->id_string = ft_strjoin(ip->id_string, val);
-		}
-		else
-			ft_charjoin(&ip->id_string, ip->ch);
-	}
-}
-
-static void single_quote(t_dlist **line, t_ip *ip)
-{
-	while (next_ch(line, ip) != '\'')
-	{
-		if (ip->ch == '\0')
-			break;
-		ft_charjoin(&ip->id_string, ip->ch);
-	}
-}
-
 static void escape_character(t_dlist **line, t_ip *ip)
 {
 	t_dlist *line2;
@@ -40,9 +12,56 @@ static void escape_character(t_dlist **line, t_ip *ip)
 	{
 		prompt("> ", &line2);
 		ft_dlstadd_back(line, line2);
+		next_ch(line, ip);
 	}
 	else
 		ft_charjoin(&ip->id_string, ip->ch);
+}
+
+static void double_quote(t_dlist **line, t_ip *ip)
+{
+	char *val;
+	t_dlist *line2;
+
+	while (next_ch(line, ip) != '\"')
+	{
+		if (ip->ch == '\0')
+		{
+			prompt("> ", &line2);
+			ft_dlstadd_back(line, ft_dlstnew("\n"));
+			ft_dlstadd_back(line, line2);
+			next_ch(line, ip);
+		}
+		else if (ip->ch == '$')
+		{
+			val = expand_parameter(line);
+			ip->id_string = ft_strjoin(ip->id_string, val);
+		}
+		else if (ip->ch == '\\')
+			escape_character(line, ip);
+		else
+			ft_charjoin(&ip->id_string, ip->ch);
+	}
+}
+
+static void single_quote(t_dlist **line, t_ip *ip)
+{
+	t_dlist *line2;
+
+	while (next_ch(line, ip) != '\'')
+	{
+		if (ip->ch == '\0')
+		{
+			prompt("> ", &line2);
+			ft_dlstadd_back(line, ft_dlstnew("\n"));
+			ft_dlstadd_back(line, line2);
+			next_ch(line, ip);
+		}
+		else if (ip->ch == '\\')
+			escape_character(line, ip);
+		else
+			ft_charjoin(&ip->id_string, ip->ch);
+	}
 }
 
 void quoting(t_dlist **line, t_ip *ip)
