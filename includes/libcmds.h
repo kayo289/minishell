a3c34fdfc009c8616be14c9d061834e6d30255ee
kayo289 <kayokapi@icloud.com>
2021-04-20 02:48:38 +0900

    add unset.c

--- includes/libcmds.h
+++ includes/libcmds.h
@@ -1,4 +1,4 @@
-#ifndef LIBCMDS_H 
+#ifndef LIBCMDS_H
 #define LIBCMDS_H
 
 #include <stdio.h>
@@ -9,9 +9,28 @@
 #include <stdbool.h>
 #include "libft.h"
 
+#define SIZE	29999
+
+struct s_shell;
+struct s_param;
+typedef struct s_param		t_param;
+typedef struct s_shell *	t_shell;
+
+struct s_shell
+{
+	t_list	*var[SIZE];
+	int		exit_status;
+};
+
+struct s_param
+{
+	char *key;
+	char *value;
+};
+
 void cd(char **argv);
 void pwd(char **argv);
 void echo(char **argv);
-
+void unset(char **argv, t_shell *shell);
 
 #endif
--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -1,14 +1,42 @@
-#include "../../includes/minishell.h"
+#include "../../includes/libcmds.h"
 
-void unset(int argc, char **argv)
+static int hash(char *name)
 {
 	int i;
+	int h;
 
-	if (argc <= 1)
-		return ;
-	while (argv[i])
+	h = 0;
+	i = 0;
+	while (name[i] != '\0')
 	{
-		ft_unsetenv(argv[i]);
+		h = h * 10 + name[i];
+		h %= SIZE;
 		i++;
 	}
+	return (h);
+}
+
+static char *del_shell_var(t_shell this, char *name)
+{
+	t_list *top;
+	t_list *tmp;
+	int h;
+
+	h = hash(name);
+	top = this->var[h];
+	while (top->next != NULL && ft_strcmp(((t_param*)top->next->content)->key, name) != 0)
+		top = top->next;
+	if (top->next == NULL)
+		return (NULL);
+	tmp = top->next;
+	top->next = tmp->next;
+	free(tmp);
+	return (NULL);
+}
+
+void unset(char **argv, t_shell *shell)
+{
+	if (argv[1])
+		ft_unsetenv(argv[1]);
+	del_shell_var(*shell, argv[1]);
 }
