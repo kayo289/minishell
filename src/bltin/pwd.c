739e1fbda182a84baf14e33c490bb10332a8eb60
kayo289 <kayokapi@icloud.com>
2021-04-28 21:45:16 +0900

    fix pwd argument err

--- src/bltin/pwd.c
+++ src/bltin/pwd.c
@@ -5,7 +5,17 @@ int minishell_pwd(char **argv)
 	char *path;
 
 	path = getcwd(NULL, 0);
+	if (argv[1] != NULL)
+	{
+		ft_putendl_fd("pwd: too many arguments", 1);
+		return (1);
+	}
+	if (path == NULL)
+	{
+		ft_putendl_fd(strerror(errno), 2);
+		exit(EXIT_FAILURE);
+	}
 	ft_putendl_fd(path, 1);
-	(void)argv;
+	free(path);
 	return (0);
 }
--- src/cmds/pwd.c
+++ src/cmds/pwd.c
@@ -5,6 +5,11 @@ int main(int argc, char **argv)
 	char *path;
 
 	path = getcwd(NULL, 0);
+	if (argc != 1)
+	{
+		ft_putendl_fd("pwd: too many arguments", 1);
+		exit(EXIT_FAILURE);
+	}
 	if (path == NULL)
 	{
 		ft_putendl_fd(strerror(errno), 2);
@@ -13,6 +18,5 @@ int main(int argc, char **argv)
 	ft_putendl_fd(path, 1);
 	free(path);
 	return (0);
-	(void)argc;
 	(void)argv;
 }
