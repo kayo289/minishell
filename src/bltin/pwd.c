a3fd5278011900cb1feee3ce6340ab600404a5c4
kayo289 <kayokapi@icloud.com>
2021-04-28 22:19:11 +0900

    fix pwd code

--- src/bltin/pwd.c
+++ src/bltin/pwd.c
@@ -5,7 +5,13 @@ int minishell_pwd(char **argv)
 	char *path;
 
 	path = getcwd(NULL, 0);
+	if (path == NULL)
+	{
+		ft_putendl_fd(strerror(errno), 2);
+		exit(EXIT_FAILURE);
+	}
 	ft_putendl_fd(path, 1);
-	(void)argv;
+	free(path);
 	return (0);
+	(void)argv;
 }
