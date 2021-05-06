6d2844447ad46127df0957faaa4ad405f14e1108
rishimot <rishimot@student.42.tokyo>
2021-05-06 16:54:30 +0200

    [fix] exit 1

--- src/bltin/exit.c
+++ src/bltin/exit.c
@@ -9,7 +9,7 @@ void exit_err(char *msg)
 int minishell_exit(char **argv, t_shell *shell)
 {
 	int i;
-	int n;
+	long int n;
 	int minus;
 
 	ft_putendl_fd("exit", 2);
@@ -24,23 +24,23 @@ int minishell_exit(char **argv, t_shell *shell)
 		i = 0;
 		if (argv[1][i] == '-')
 		{
-			i++;
 			minus = -1;
+			i++;
 		}
 		n = 0;
 		while (argv[1][i] != '\0')
 		{
 			if (ft_isdigit(argv[1][i]))
 			{
-				if (n * minus > (INT_MAX - (argv[1][i] - '0')) / 10 ||\
-					n * minus < (INT_MIN - (argv[1][i] - '0')) / 10)
-					exit_err("numeric argument required");
 				n = n * 10 + (argv[1][i] - '0');
+				if (n * minus > INT_MAX || n * minus < INT_MIN)
+					exit_err("numeric argument required");
 			}
 			else
 				exit_err("numeric argument required");
 			i++;
 		}
+		printf("%ld\n", n);
 		shell->exit_status = n * minus;
 	}
 	exit(shell->exit_status);
