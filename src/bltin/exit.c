dd6206a6d4d25fd1511ff4b486bcc4aaa55e4f07
Kayo Kikuchi <kkikuchi@c1r4s2.42tokyo.jp>
2021-05-07 03:49:02 +0900

    exit a

--- src/bltin/exit.c
+++ src/bltin/exit.c
@@ -1,16 +1,21 @@
 #include "../../includes/minishell.h"
 
-void exit_err(char *msg)
+void exit_err(char *s, char *msg)
 {
-	err_cstmmsg("exit", msg);
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd("exit: ", 2);
+	ft_putstr_fd(s, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd(msg, 2);
 	exit(255);
 }
 
 int minishell_exit(char **argv, t_shell *shell)
 {
 	int i;
-	long int n;
+	long long int n;
 	int minus;
+	char *str;
 
 	ft_putendl_fd("exit", 2);
 	if (argv[1] != NULL)
@@ -28,16 +33,20 @@ int minishell_exit(char **argv, t_shell *shell)
 			i++;
 		}
 		n = 0;
+		str = "";
 		while (argv[1][i] != '\0')
 		{
+			str = ft_strjoin(str, &argv[1][i]);
 			if (ft_isdigit(argv[1][i]))
 			{
-				n = n * 10 + (argv[1][i] - '0');
-				if (n * minus > INT_MAX || n * minus < INT_MIN)
-					exit_err("numeric argument required");
+				if (n * minus <= (LLONG_MAX - (long long)(argv[1][i] - '0')) / 10 ||
+					n * minus >= (LLONG_MIN - (long long)(argv[1][i] - '0')) / 10)
+					n = n * 10 + (argv[1][i] - '0');
+				else
+					exit_err(str, "numeric argument required");
 			}
 			else
-				exit_err("numeric argument required");
+				exit_err(str, "numeric argument required");
 			i++;
 		}
 		shell->exit_status = n * minus;
