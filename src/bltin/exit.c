77615b8ca6229fa3042377c330f342480737b10e
Ryo Ishimoto <rishimot@c1r29s10.42tokyo.jp>
2021-05-06 23:27:36 +0900

    [fix] exit -1

--- src/bltin/exit.c
+++ src/bltin/exit.c
@@ -4,29 +4,36 @@ int minishell_exit(char **argv, t_shell *shell)
 {
 	int i;
 	int n;
+	int minus;
 
 	ft_putendl_fd("exit", 2);
 	if (argv[1] != NULL)
 	{
 		if (argv[2] != NULL)
 		{
-			ft_putendl_fd("exit: too many arguments", 2);
+			err_cstmmsg("exit", "too many arguments");
 			return (1);
 		}
 		n = 0;
+		minus = 1;
 		i = -1;
+		if (argv[1][i] != '-')
+		{
+			i++;
+			minus = -1;
+		}
 		while (argv[1][++i] != '\0')
 		{
 			if (ft_isdigit(argv[1][i]))
 				n = n * 10 + (argv[1][i] - '0');
 			else
 			{
-				ft_putendl_fd("exit: numeric arguments required", 2);
+				err_cstmmsg("exit", "numeric arguments required");
 				n = 255;
 				break;
 			}
 		}
-		shell->exit_status = n;
+		shell->exit_status = n * minus;
 	}
 	exit(shell->exit_status);
 	return (0);
