7f04566026967329e6d5a5243e1caddf1400cbf9
Ryo Ishimoto <rishimot@c1r4s2.42tokyo.jp>
2021-09-24 17:15:45 +0900

    Completed:simple_command case

--- src/error/error.c
+++ src/error/error.c
@@ -2,6 +2,7 @@
 
 void err_syntax(t_ip *ip, t_shell *shell)
 {
+	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd("syntax error near unexpected token ", 2);
 	if (ip->id_string[0] == '\0')
 		ft_putendl_fd("`newline'", 2);
@@ -12,6 +13,7 @@ void err_syntax(t_ip *ip, t_shell *shell)
 
 void err_notfound(char *cmd, t_shell *shell)
 {
+	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putendl_fd(": command not found", 2);
 	shell->exit_status = 127;
@@ -20,6 +22,7 @@ void err_notfound(char *cmd, t_shell *shell)
 
 void err_badfd(int n, t_shell *shell)
 {
+	ft_putstr_fd("minishell: ", 2);
 	ft_putnbr_fd(n, 2);
 	ft_putendl_fd(": Bad file descriptor", 2);
 	shell->exit_status = 1;
