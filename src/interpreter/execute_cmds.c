448c2fb23a9af9f6e5a90c707164e9ed89e79f5e
kayo289 <kayokapi@icloud.com>
2021-10-18 00:11:39 +0900

    fix find_command

--- src/interpreter/execute_cmds.c
+++ src/interpreter/execute_cmds.c
@@ -50,19 +50,45 @@ static void		find_command(char *cmd, char **cmd_path, char **dir_names)
 		dp = readdir(dir);
 		while (dp != NULL)
 		{
-			if (stat(dp->d_name, &sb) < 0)
+			if (ft_strcmp(cmd, dp->d_name) == EQUAL)
 			{
-				if (ft_strcmp(cmd, dp->d_name) == EQUAL)
+				if (stat(dp->d_name, &sb) < 0)
 				{
 					*cmd_path = ft_strjoin(dir_names[i], "/");
 					*cmd_path = ft_strjoin(*cmd_path, dp->d_name);
-					return;
 				}
+				else
+					*cmd_path = ft_strjoin(dir_names[i],dp->d_name);
+				// execve
+				return;
 			}
 			dp = readdir(dir);
 		}
 		closedir(dir);
 	}
+}
+
+char **ft_split_path(char *str)
+{
+	char **result;
+	char *next;
+
+	result = ft_calloc2(sizeof(char*), 1);
+	next = ft_strchr(str, ':');
+	while(next)
+	{
+		if (next - str == 0)
+			result = ft_realloc2(result, ft_strdup("./"));
+		else
+			result = ft_realloc2(result, ft_substr(str, 0, next - str));
+		str = next + 1;
+		next = ft_strchr(str, ':');
+	}
+	if (*str == '\0')
+		result = ft_realloc2(result, ft_strdup("./"));
+	else
+		result = ft_realloc2(result, ft_strdup(str));
+	return (result);
 }
 
 static char	*search_path(char *cmd_name, t_shell *shell)
@@ -73,13 +99,13 @@ static char	*search_path(char *cmd_name, t_shell *shell)
 
 	cmd_path = NULL;
 	env_value = get_param_value(get_shell_var(shell, "PATH"));
-	if (env_value == NULL || *env_value == '\0') 
+	if (env_value == NULL || *env_value == '\0')
 	{
 		error_message(strerror(ENOENT), cmd_name);
 		shell->exit_status = 127;
 		minishell_end(shell);
 	}
-	dir_names = ft_split(env_value, ':');
+	dir_names = ft_split_path(env_value);
 	find_command(cmd_name, &cmd_path, dir_names);
 	dp_free(dir_names);
 	return (cmd_path);
@@ -94,14 +120,15 @@ void	execute_cmds(char **args, t_shell *shell)
 	cmd = args[0];
 	if (ft_strchr(cmd, '/') != NULL)
 		cmd_path = ft_strdup(cmd);
-	else 
+	else
 	{
 		cmd_path = search_path(cmd, shell);
-		if (cmd_path == NULL)
+		if (errno && cmd_path)
+			err_cstmmsg(cmd_path, NULL, strerror(errno));
+		else
 			err_notfound(cmd, shell);
 	}
 	execve(cmd_path, args, environ);
 	handle_execve_error(cmd_path, shell);
 	free(cmd_path);
 }
-
